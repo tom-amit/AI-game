d0728c3465919f3243da4490bd82f27f79b7b80e
amits6666 <amit.sivan6666@gmail.com>
2022-01-28 18:35:06 +0200

    fixed wait for visuals

--- GameVisuals.cs
+++ GameVisuals.cs
@@ -104,10 +104,31 @@ namespace PawnGame
                 }
             }
         }
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
 
+            // Console.WriteLine("start wait timer");
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // Console.WriteLine("stop wait timer");
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        } //Wait function 
         private void CompPlay()
         {
-            System.Threading.Thread.Sleep(50);
+            wait(50);
             if (!inPlacement)
             {
                 if (isAI1 && board.turn == 0)
